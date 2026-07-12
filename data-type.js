f83274d68c65c6c73fa292ba7950e19b2c86a7e0
Riaz-45 <rakibulhasanriaz1234@gmail.com>
2026-07-13 01:36:50 +0600

    added some files

---
+++ data-type.js
@@ -0,0 +1,19 @@
+// JS is a dynamic typed programming language
+let date = 4;
+data = 'name';
+data = false;
+data = {name: 'raja', price: 40};
+data = [455];
+data = undefined;
+data = null;
+
+
+// JS: primitive data type
+const price = 50;
+const name = 'border';
+const isPoor = false;
+let isKnown = undefined; //usually you should not set value to undefined
+
+// JS: non-primitive data type
+const student = {name: 'babul', location: 'bogura', subject: 'botany'};
+const friends = ['tom', 'bob', 'jack', 'rock'];
