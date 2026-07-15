48affd77af312f773206882fe476174780351ca4
Riaz-45 <rakibulhasanriaz1234@gmail.com>
2026-07-15 10:53:12 +0600

    added files

---
+++ callback.js
@@ -0,0 +1,16 @@
+function pakhiBhai(callMeBack, patro, patri){
+    console.log('callMeBack parameter', callMeBack);
+    // if(patri){
+    //     console.log('call me back');
+    // }
+}
+
+// pakhiBhai()
+
+function callSomeOne(person){
+    console.log('calling', person);
+}
+
+// callSomeOne('jodu');
+
+pakhiBhai(callSomeOne);
---
+++ pass-by-reference.js
@@ -0,0 +1,45 @@
+// function call for primitive data
+function multiply(x, y){
+    x = x + 5;
+    y = y + 5;
+    const mult = x*y;
+    return mult;
+}
+
+const a = 5;
+const b = 7;
+
+console.log('before the function call: ', a, b);
+const result = multiply(a, b);
+console.log(result);
+console.log('after the function call: ', a, b);
+
+
+// function call for non-primitive values (object, array) are passed by reference
+const manik = {name: 'manik', balance: 5000};
+const roton = {name: 'roton', balance: 40000};
+
+console.log('before call', manik, roton);
+
+function totalMoney(person1, person2){
+    person1.balance = 0;
+    person2.balance = person2.balance / 2;
+    const total = person1.balance + person2.balance;
+    return total;
+}
+
+const balance = totalMoney(manik, roton);
+console.log('balance', balance);
+
+console.log('after function call', manik, roton);
+
+
+
+// function Arguments
+function add(a, b){
+    console.log(arguments);
+    const params = [...arguments];
+    console.log(params);
+}
+
+add(45, 10, 34, 23);
